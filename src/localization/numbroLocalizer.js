8f0d2efe8e95971ee449fa27dd00fc2e8f522455
André Pena <andrerpena@gmail.com>
2016-09-21 10:23:59 -0300

    #91 fixing 0 being considered empty

--- src/localization/numbroLocalizer.js
+++ src/localization/numbroLocalizer.js
@@ -15,7 +15,7 @@ export default function(numbro) {
             if(format) throw Error('numbro localizer\'s parse method does not support passing the format');
             if(culture) throw Error('numbro localizer\'s parse method does not support passing the culture');
 
-            if (value == null || value == undefined || value == '') return undefined; // localizers should return undefined for empty inputs and null for invalid inputs
+            if (value === null || value === undefined || value === '') return undefined; // localizers should return undefined for empty inputs and null for invalid inputs
 
             // numbro's unformat is very permissive, so we need to check for a valid number
             if(/^[+-]?[0-9,\.]+$/.test(value)) { // ToDo: improve this regular expression
